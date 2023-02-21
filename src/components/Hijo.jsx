599ba03d7ef1ae3f9772c61060dd11cee921392b
Juan Lopez <jucalo2195@gmail.com>
2023-02-21 17:43:26 -0500

    Creacion json

---
+++ src/components/Hijo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './card.css'
+
+export const Hijo = () => {
+return (
+    <div className="card">
+        <h2>Titulo</h2>
+        <img src="url imagen" alt="No sirve"/>
+        <p>Texto</p>
+        <button>Detalle</button>
+    </div>
+)
+}
---
+++ src/components/Hijo2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Hijo2 = () => {
+return (
+    <div className='footer-box'>
+        <button>Incrementar</button>
+        <button>Decrementar</button>
+        <button>Resetear</button>
+        <p className="contador">contador</p>
+    </div>
+)
+}
