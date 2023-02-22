bb8b249689776f9c0477d1f495a2369b03dfb602
Juan Lopez <jucalo2195@gmail.com>
2023-02-22 14:38:25 -0500

    Commit 2

--- src/components/Hijo.jsx
+++ src/components/Hijo.jsx
@@ -6,6 +6,7 @@ return (
     <div className="card">
         <h2>Titulo</h2>
         <img src="url imagen" alt="No sirve"/>
+        <h4>contador:</h4>
         <p>Texto</p>
         <button>Detalle</button>
     </div>
